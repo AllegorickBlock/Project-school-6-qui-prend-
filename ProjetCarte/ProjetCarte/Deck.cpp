aaf48e3dae2611e2abaecf06d15d6b215aa71388
LA170005 Jason Bourlet <la170005@student.helha.be>
2018-11-23 14:24:16 +0100

    Création Deck

---
+++ ProjetCarte/ProjetCarte/Deck.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Deck.h"
+
+
+Deck::Deck()
+{
+
+	for (int i = 0; i <= 104; i++)
+	{
+		Card
+	}
+}
+
+
+Deck::~Deck()
+{
+}
---
+++ ProjetCarte/ProjetCarte/Deck.h
@@ -0,0 +1,14 @@
+#pragma once
+class Deck
+{
+	int tab_Cards[104];
+
+
+public:
+	Deck();
+	~Deck();
+
+	void Mix_card();
+
+};
+

1cc062e02b8cf1d2f0d0ae39d1ed1ab0f5d52386
Martin Beaussart <la163852@student.helha.be>
2018-11-24 09:30:43 +0100

    Mix-card(), creation de code mais presence d'erreurs

--- ProjetCarte/ProjetCarte/Deck.cpp
+++ ProjetCarte/ProjetCarte/Deck.cpp
@@ -22,4 +22,28 @@ Deck::~Deck() // Destructeur necessaire pour detruire toutes les carte créé dyna
 	}
 }
 
-Card Deck::get_Card(int some_Card) { return *tab_Cards[some_Card]; } 
+Card Deck::get_Card(int some_Card) { return *tab_Cards[some_Card]; }
+void Deck::Mix_card()
+{
+	srand(time(0)); // Prend un temps random comme valeur, permet de s'assurer d'avoir des chifres differents a chaque compilation
+	int random_number;
+	Card *store_cards[104];
+	for (int i = 0; i < 104; i++)
+	{ 
+		Card *my_card = new Card(i + 1);
+		store_cards[i] = my_card;	
+	}
+
+	for (int j = 0; j < 104; j++)
+	{
+		random_number = ( (rand() % 104) + 1 );
+		if (store_cards[random_number] != nullptr)
+		{
+			tab_Cards[j] = store_cards[random_number];
+			delete store_cards[j];
+			store_cards[random_number] = nullptr;
+		}
+		else j--;
+	}
+	delete[] store_cards;
+}
--- ProjetCarte/ProjetCarte/Deck.h
+++ ProjetCarte/ProjetCarte/Deck.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Card.h"
+#include <ctime> //permet d'utiliser time()
+#include <cstdlib> //Permet d'utiliser srand()
 class Deck
 {
 	Card *tab_Cards[104]; // Pointeur renvoyant un tableau de differentes adresse ou l'on souhaitera stoquer nos objets Card

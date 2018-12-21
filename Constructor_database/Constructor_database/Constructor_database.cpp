97cac2d212dafee72e8c8a753ec589c4ddfef805
Martin Beaussart <la163852@student.helha.be>
2018-12-21 15:13:41 +0200

    test connection à score.db

--- Constructor_database/Constructor_database/Constructor_database.cpp
+++ Constructor_database/Constructor_database/Constructor_database.cpp
@@ -5,22 +5,28 @@
 #include <iostream>
 #include "sqlite3.h"
 
+using namespace std;
+
 int main()
 {
-	sqlite3* Score;
-	std::string sql = "CREATE TABLE Player("
-		"ID INT PRIMARY KEY     NOT NULL) ";
-	int exit = 0;
-	exit = sqlite3_open("example.db", &Score);
-	char* messaggeError;
-	exit = sqlite3_exec(Score, sql.c_str(), NULL, 0, &messaggeError);
+	int a;
 
-	if (exit != SQLITE_OK) {
-		std::cerr << "Error Create Table" << std::endl;
-		sqlite3_free(messaggeError);
-	}
-	else
-		std::cout << "Table created Successfully" << std::endl;
-	sqlite3_close(Score);
+
+
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open("score.db", &db);
+
+	if (rc)	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	else	fprintf(stderr, "Opened database successfully\n");
+
+	sqlite3_close(db);
+
+
+
+
+	cin >> a;
 	return (0);
 }

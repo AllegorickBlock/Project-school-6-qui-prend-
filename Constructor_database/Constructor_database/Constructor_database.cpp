2dd36e331a8408c40e00a5aa5b7d9c4813c3f0ee
Martin Beaussart <la163852@student.helha.be>
2018-12-21 17:01:44 +0200

    modifications

--- Constructor_database/Constructor_database/Constructor_database.cpp
+++ Constructor_database/Constructor_database/Constructor_database.cpp
@@ -2,6 +2,8 @@
 //
 
 #include "pch.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include "sqlite3.h"
 
@@ -9,22 +11,20 @@ using namespace std;
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 	int i;
-	for (i = 0; i < argc; i++) printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
 	printf("\n");
 	return 0;
 }
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
 
-
-	int a = 0;
-
-
+	int z = 0;
 	sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
-	char *sql;
+	const char *sql;
 
 	/* Open database */
 	rc = sqlite3_open("score.db", &db);
@@ -33,18 +33,12 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		return(0);
 	}
-	else fprintf(stderr, "Opened database successfully\n");
-	
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
 
 	/* Create SQL statement */
-	sql = "INSERT INTO Player (IDplayer)\n"  
-		"VALUES (1); \n" \
-		"INSERT INTO Player (IDplayer) \n"  \
-		"VALUES (2); \n"     \
-		"INSERT INTO Player (IDplayer)\n" \
-		"VALUES (3);\n" \
-		"INSERT INTO Player (IDplayer)\n" \
-		"VALUES (4);";
+	sql = "INSERT INTO Player VALUES (1) ";
 
 	/* Execute SQL statement */
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -53,9 +47,11 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
-	else fprintf(stdout, "Records created successfully\n");
-
+	else {
+		fprintf(stdout, "Records created successfully\n");
+	}
 	sqlite3_close(db);
-	cin >> a;
 	return 0;
+
+	cin >> z;
 }
